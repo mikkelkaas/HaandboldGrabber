0fea175a5fd70aba8d1e3e22131ca4e729a96416
Mikkel Kaas Frich <mkf@nemlig.com>
2023-10-05 13:12:56 +0200

    Add league

--- HaandboldGrabber/Helpers/CalendarConverter.cs
+++ HaandboldGrabber/Helpers/CalendarConverter.cs
@@ -15,7 +15,7 @@ public static class CalendarConverter
             Summary = match.Team1 + " - " + match.Team2,
             Location = match.TvChannel,
             Start = new CalDateTime(match.GameTime, "Europe/Copenhagen"),
-
+            Description = match.League,
             End = new CalDateTime(match.GameTime.AddHours(2), "Europe/Copenhagen"),
             IsAllDay = false
         };
